2f0ed061dc15d20000d48163f8958dfea3e16505
swedesjs <babaevvladislav828@gmail.com>
2021-10-20 20:36:47 +0300

    added `Intent` class serving for parameters in msg

---
+++ lib/src/models/objects/intent.dart
@@ -0,0 +1,13 @@
+part of vk_library;
+
+class Intent {
+  static const PROMO_NEWLETTER = Intent("promo_newsletter");
+  static const NON_PROMO_NEWLETTER = Intent("non_promo_newsletter");
+  static const CONFIRMED_NOTIFICATION = Intent("confirmed_notification");
+  
+  final String intent;
+  const Intent(this.intent);
+
+  @override
+  String toString() => intent;
+}
