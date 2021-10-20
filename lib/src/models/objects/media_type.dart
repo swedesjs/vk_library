4c997aa5a496e45a46ed2c420d7fb7a1f3996f92
swedesjs <babaevvladislav828@gmail.com>
2021-10-20 20:23:46 +0300

    added class MediaType that describes type in VK

---
+++ lib/src/models/objects/media_type.dart
@@ -0,0 +1,33 @@
+part of vk_library;
+
+class MediaType {
+  /// Photographs
+  static const PHOTO = MediaType("photo");
+  
+  /// Videos
+  static const VIDEO = MediaType("video");
+
+  /// Audio recordings
+  static const AUDIO = MediaType("audio");
+
+  /// Documents
+  static const DOC = MediaType("doc");
+
+  /// Links
+  static const LINK = MediaType("link");
+
+  /// Goods
+  static const MARKET = MediaType("market");
+
+  /// Records
+  static const WAll = MediaType("wall");
+
+  /// Links, products and posts
+  static const SHARE = MediaType("share");
+
+  final String type;
+  const MediaType(this.type);
+
+  @override
+  String toString() => type;
+}
