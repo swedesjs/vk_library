0af0432f8fe9dbd645384496c902c043acad9088
swedesjs <babaevvladislav828@gmail.com>
2021-10-22 21:59:24 +0300

    Executing todo for interface `getChatPreview`

--- lib/src/models/methods/returned/messages/getChatPreview.dart
+++ lib/src/models/methods/returned/messages/getChatPreview.dart
@@ -1,5 +1,40 @@
 part of vk_library;
 
+/// Interface for the [ReturnedMessagesGetChatPreview.preview] field
+class MessagesGetChatPreview {
+  /// The object that is used for the interface
+  final Json object;
+  const MessagesGetChatPreview(this.object);
+
+  /// Chat creator id.
+  int get adminId => object["admin_id"];
+
+  /// Array of chat participants ids.
+  List<int> get members => object["members"];
+
+  /// The name of the chat.
+  String get title => object["title"];
+
+  /// Chat cover.
+  PhotoObject get photo => PhotoObject(object["photo"]);
+
+  /// Chat ID for the current user.
+  int get localId => object["local_id"];
+}
+
+/// Interface for the [ReturnedMessagesGetChatPreview.emails] field
+class MessagesGetChatEmails {
+  /// The object that is used for the interface
+  final Json object;
+  const MessagesGetChatEmails(this.object);
+
+  /// E-mail identifier.
+  int get id => object["id"];
+
+  /// Email address.
+  String get address => object["address"];
+}
+
 /// Interface for the `messages.getChatPreview` method
 class ReturnedMessagesGetChatPreview {
   /// The object that is used for the interface
@@ -7,8 +42,8 @@ class ReturnedMessagesGetChatPreview {
   const ReturnedMessagesGetChatPreview(this.object);
 
   /// Chat info.
-  // TODO: Create a class for this object
-  Json get preview => object["preview"];
+  MessagesGetChatPreview get preview =>
+      MessagesGetChatPreview(object["preview"]);
 
   /// Array of objects [users](https://vk.com/dev/objects/user).
   List<ProfileObject> get profiles => (object["profiles"] as List)
@@ -21,6 +56,8 @@ class ReturnedMessagesGetChatPreview {
   List<Json> get groups => object["groups"];
 
   /// An array of objects describing e-mail.
-  // TODO: Write a class describing emails with fields id ([int]) and address ([String])
-  List<Json> get emails => object["emails"];
+  List<MessagesGetChatEmails> get emails => (object["emails"] as List)
+      .cast<Json>()
+      .map((e) => MessagesGetChatEmails(e))
+      .toList();
 }
