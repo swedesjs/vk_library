0a4e4a690e3596012f2f21a39966970552cadaf2
swedesjs <babaevvladislav828@gmail.com>
2021-10-23 21:06:02 +0300

    Class implementation has become shorter

--- lib/src/models/methods/returned/messages/getChat.dart
+++ lib/src/models/methods/returned/messages/getChat.dart
@@ -2,9 +2,5 @@ part of vk_library;
 
 /// Interface for the `messages.getChat` method
 class ReturnedMessagesGetChat extends ChatObjects {
-  /// The object that is used for the interface
-  @override
-  final Json object;
-
-  const ReturnedMessagesGetChat(this.object) : super(object);
+  const ReturnedMessagesGetChat(Json object) : super(object);
 }
