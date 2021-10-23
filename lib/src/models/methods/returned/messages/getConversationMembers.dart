e6df119df65f0c9f349c9b9c2c23854bfaac4234
swedesjs <babaevvladislav828@gmail.com>
2021-10-23 12:30:23 +0300

    Add an interface for the GetCon... interface field

--- lib/src/models/methods/returned/messages/getConversationMembers.dart
+++ lib/src/models/methods/returned/messages/getConversationMembers.dart
@@ -1,5 +1,27 @@
 part of vk_library;
 
+/// Interface for the [ReturnedMessagesGetConversationMembers.items] field
+class MessagesGetConversationMembersItems {
+  /// The object that is used for the interface
+  final Json object;
+  const MessagesGetConversationMembersItems(this.object);
+
+  /// Conversation participant ID.
+  int get memberId => object["member_id"];
+
+  /// The ID of the user who invited the participant.
+  int get invitedBy => object["invited_by"];
+
+  /// Date added to conversation.
+  int get joinDate => object["join_date"];
+
+  /// Whether the user is an administrator.
+  bool get isAdmin => object["is_admin"];
+
+  /// Whether the current user can exclude a member.
+  bool get canKick => object["can_kick"];
+}
+
 /// Interface for the `messages.getConversationMembers` method
 class ReturnedMessagesGetConversationMembers {
   /// The object that is used for the interface
@@ -11,8 +33,11 @@ class ReturnedMessagesGetConversationMembers {
   int get count => object["count"];
 
   /// Participants of the conversation. Array of objects
-  // TODO: Create a class describing the participants in the conversation in this method
-  List<Json> get items => object["items"];
+  List<MessagesGetConversationMembersItems> get items =>
+      (object["items"] as List)
+          .cast<Json>()
+          .map((e) => MessagesGetConversationMembersItems(e))
+          .toList();
 
   /// Array of objects [users](https://vk.com/dev/objects/user).
   List<ProfileObject> get profiles => (object["profiles"] as List)
