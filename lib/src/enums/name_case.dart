2e1b93ea0ccae95e787d40c9342041e5dbe57375
swedesjs <babaevvladislav828@gmail.com>
2021-10-27 22:44:32 +0300

    Explicit type indication in case map.

--- lib/src/enums/name_case.dart
+++ lib/src/enums/name_case.dart
@@ -1,21 +1,27 @@
 part of vk_library;
+
 /// Enum is a list of cases.
 enum NameCase {
   /// Nominative.
   NOM,
+
   /// Genitive.
   GEN,
+
   /// Dative.
   DAT,
+
   /// Accusative.
   ACC,
+
   /// Instrumental.
   INS,
+
   /// Prepositional.
   ABL
 }
 
-const _nameCaseString = {
+const _nameCaseString = <NameCase, String>{
   NameCase.NOM: "nom",
   NameCase.GEN: "gen",
   NameCase.DAT: "dat",
