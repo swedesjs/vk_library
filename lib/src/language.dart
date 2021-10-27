76664cc835b12c30ccdfc9bb0e4a4734534e2827
swedesjs <babaevvladislav828@gmail.com>
2021-10-27 11:26:31 +0300

    The Language class has been replaced by the enum.

--- lib/src/language.dart
+++ lib/src/language.dart
@@ -1,19 +1,50 @@
 part of vk_library;
 
-/// The class is used for the [VK], [API] classes as the `language` parameter, for example:
-/// ```dart
-/// final vk = VK(token: "token", language: Language.RU);
-/// ```
-class Language {
-  /// Russian language
-  static const RU = Language("ru");
+/// Enum is a list of languages ​​in which VK can provide you with data! Used in class [VK] and [API]
+enum Language {
+  /// Russian.
+  RU,
 
-  /// The format of the string sent to VK
-  final String language;
+  /// Ukrainian.
+  UK,
 
-  /// It is not advisable to create an instance
-  const Language(this.language);
+  /// Belorussian
+  BE,
 
-  @override
-  String toString() => language;
+  /// English.
+  EN,
+
+  /// Spanish.
+  ES,
+
+  /// Finnish.
+  FI,
+
+  /// German.
+  DE,
+
+  /// Italian.
+  IT
 }
+
+const _languageEnum = <Language, String>{
+  Language.RU: "ru",
+  Language.UK: "uk",
+  Language.BE: "be",
+  Language.EN: "en",
+  Language.ES: "es",
+  Language.FI: "fi",
+  Language.DE: "de",
+  Language.IT: "it"
+};
+
+const _languageString = <String, Language>{
+  "ru": Language.RU,
+  "uk": Language.UK,
+  "be": Language.BE,
+  "en": Language.EN,
+  "es": Language.ES,
+  "fi": Language.FI,
+  "de": Language.DE,
+  "it": Language.IT
+};
