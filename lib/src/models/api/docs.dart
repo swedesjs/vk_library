dcb9943dd609067cb8a15ede1999f2d82610bbf5
swedesjs <babaevvladislav828@gmail.com>
2021-11-02 22:54:19 +0300

    Snake_case converted to camelCase.

--- lib/src/models/api/docs.dart
+++ lib/src/models/api/docs.dart
@@ -9,24 +9,24 @@ class Docs {
 
   /// Copies the document to the documents of the current user.
   Future<int> add({
-    required int owner_id,
-    required int doc_id,
-    String? access_key,
+    required int ownerId,
+    required int docId,
+    String? accessKey,
   }) async {
     final data = await _callMethod("add", {
-      "owner_id": owner_id,
-      "doc_id": doc_id,
-      if (access_key != null) "access_key": access_key,
+      "owner_id": ownerId,
+      "doc_id": docId,
+      if (accessKey != null) "access_key": accessKey,
     });
 
     return data.response;
   }
 
   /// Deletes a user or group document.
-  Future<bool> delete({required int owner_id, required int doc_id}) async {
+  Future<bool> delete({required int ownerId, required int docId}) async {
     final data = await _callMethod("delete", {
-      "owner_id": owner_id,
-      "doc_id": doc_id,
+      "owner_id": ownerId,
+      "doc_id": docId,
     });
 
     return data.response == 1;
@@ -34,14 +34,14 @@ class Docs {
 
   /// Edits a user or group document.
   Future<bool> edit({
-    required int owner_id,
-    required int doc_id,
+    required int ownerId,
+    required int docId,
     String? title,
     List<String>? tags,
   }) async {
     final data = await _callMethod("edit", {
-      "owner_id": owner_id,
-      "doc_id": doc_id,
+      "owner_id": ownerId,
+      "doc_id": docId,
       if (title != null) "title": title,
       if (tags != null) "tags": tags.join(","),
     });
@@ -55,25 +55,25 @@ class Docs {
     int? offset,
     // TODO: Implement the enum this parameter - https://vk.com/dev/docs.get
     int? type,
-    int? owner_id,
-    bool? return_tags,
+    int? ownerId,
+    bool? returnTags,
   }) async {
     final data = await _callMethod("get", {
       if (count != null) "count": count,
       if (offset != null) "offset": offset,
       if (type != null) "type": type,
-      if (owner_id != null) "owner_id": owner_id,
-      if (return_tags != null) "return_tags": return_tags
+      if (ownerId != null) "owner_id": ownerId,
+      if (returnTags != null) "return_tags": returnTags
     });
 
     return data.response;
   }
 
   /// Returns information about documents by their IDs.
-  Future<Json> getById({List<String>? docs, bool? return_tags}) async {
+  Future<Json> getById({List<String>? docs, bool? returnTags}) async {
     final data = await _callMethod("getById", {
       if (docs != null) "docs": docs.join(","),
-      if (return_tags != null) "return_tags": return_tags,
+      if (returnTags != null) "return_tags": returnTags,
     });
 
     return data.response;
@@ -83,36 +83,36 @@ class Docs {
   Future<Json> getMessagesUploadServer({
     // TODO: Implement the enum this parameter - https://vk.com/dev/docs.getMessagesUploadServer
     String? type,
-    int? peer_id,
+    int? peerId,
   }) async {
     final data = await _callMethod("getMessagesUploadServer", {
       if (type != null) "type": type,
-      if (peer_id != null) "peer_id": peer_id,
+      if (peerId != null) "peer_id": peerId,
     });
 
     return data.response;
   }
 
   /// Returns the types of documents available to the user.
-  Future<Json> getTypes({required int owner_id}) async {
-    final data = await _callMethod("getTypes", {"owner_id": owner_id});
+  Future<Json> getTypes({required int ownerId}) async {
+    final data = await _callMethod("getTypes", {"owner_id": ownerId});
 
     return data.response;
   }
 
   /// Returns the server address for uploading documents.
-  Future<Json> getUploadServer({int? group_id}) async {
+  Future<Json> getUploadServer({int? groupId}) async {
     final data = await _callMethod("getUploadServer", {
-      if (group_id != null) "group_id": group_id,
+      if (groupId != null) "group_id": groupId,
     });
 
     return data.response;
   }
 
   /// Returns the server address for uploading documents to the Sent Items folder, for later sending the document to the wall or by personal message.
-  Future<Json> getWallUploadServer({int? group_id}) async {
+  Future<Json> getWallUploadServer({int? groupId}) async {
     final data = await _callMethod("getUploadServer", {
-      if (group_id != null) "group_id": group_id,
+      if (groupId != null) "group_id": groupId,
     });
 
     return data.response;
@@ -123,13 +123,13 @@ class Docs {
     required String file,
     String? title,
     String? tags,
-    bool? return_tags,
+    bool? returnTags,
   }) async {
     final data = await _callMethod("save", {
       "file": file,
       if (title != null) "title": title,
       if (tags != null) "tags": tags,
-      if (return_tags != null) "return_tags": return_tags
+      if (returnTags != null) "return_tags": returnTags
     });
 
     return data.response;
@@ -138,17 +138,17 @@ class Docs {
   /// Returns search results for documents.
   Future<Json> search({
     required String q,
-    bool? search_own,
+    bool? searchOwn,
     int? count,
     int? offset,
-    bool? return_tags,
+    bool? returnTags,
   }) async {
     final data = await _callMethod("search", {
       "q": q,
-      if (search_own != null) "search_own": search_own,
+      if (searchOwn != null) "search_own": searchOwn,
       if (count != null) "count": count,
       if (offset != null) "offset": offset,
-      if (return_tags != null) "return_tags": return_tags,
+      if (returnTags != null) "return_tags": returnTags,
     });
 
     return data.response;
