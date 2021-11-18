eab9213c1cdb83267a5248705170f78712110fbb
swedesjs <babaevvladislav828@gmail.com>
2021-11-18 22:16:49 +0300

    Corrected tests, and added a new test.

--- test/ad_specification_test.dart
+++ test/ad_specification_test.dart
@@ -1,46 +1,27 @@
-import "dart:convert";
-import "dart:mirrors";
+import "dart:mirrors" show reflectClass;
 
-import "package:test/test.dart";
+import "package:test/test.dart" show test, expect, equals;
 import "package:vk_library/vk_library.dart" show AdSpecification;
 
-Map<Symbol, dynamic> instance(Map<String, dynamic> params) {
-  return params.map(
-    (key, value) => MapEntry(
-      Symbol(
-        key.replaceAllMapped(
-          RegExp(r"_([a-z])"),
-          (match) => match[1]!.toUpperCase(),
-        ),
-      ),
-      value,
-    ),
-  );
-}
+import "decode.dart";
 
 void main() {
   final reflect = reflectClass(AdSpecification);
-  const params = {
-    "campaign_id": 123456,
-    "ad_format": 1,
-    "cost_type": 0,
-    "cpc": 2.00,
-    "category1_id": 1,
-    "title": "Test Title",
-    "link_url": "https://mysite.com",
-    "name": "My ad"
-  };
   test("Basic Test", () {
+    const params = {
+      "campaign_id": 123456,
+      "ad_format": 1,
+      "cost_type": 0,
+      "cpc": 2.00,
+      "category1_id": 1,
+      "title": "Test Title",
+      "link_url": "https://mysite.com",
+      "name": "My ad"
+    };
+
     expect(
-      jsonDecode(
-        reflect
-            .newInstance(const Symbol(""), const [], instance(params))
-            .reflectee
-            .toString(),
-      ),
+      decode(reflect, params),
       equals(params),
     );
   });
-
-  
 }
---
+++ test/campaign_specification_test.dart
@@ -0,0 +1,25 @@
+import "dart:mirrors" show reflectClass;
+
+import "package:test/test.dart" show test, expect, equals;
+import "package:vk_library/vk_library.dart"
+    show CampaignSpecification, TypeCampaignSpecification;
+
+import "decode.dart";
+
+void main() {
+  final reflect = reflectClass(CampaignSpecification);
+
+  test("Basic Test", () {
+    const params = {"client_id": 3};
+
+    expect(decode(reflect, params), equals(params));
+
+    const params2 = {
+      ...params,
+      "type": TypeCampaignSpecification.NORMAL,
+      "name": "Test",
+    };
+
+    expect(decode(reflect, params2), equals({...params2, "type": "normal"}));
+  });
+}
