05433d727035e8eed024f2b84ae0637ad996d510
swedesjs <babaevvladislav828@gmail.com>
2022-04-13 23:34:40 +0300

    Longpoll group class is listed in a separate file

---
+++ lib/src/longpoll/group_longpoll.dart
@@ -0,0 +1,91 @@
+part of 'longpoll.dart';
+
+/// Longpoll class for bots
+class GroupLongpoll extends Longpoll<UpdateGroupLongpoll> {
+  final API _api;
+  int? _groupId;
+
+  /// Group Id.
+  int? get groupId => _groupId;
+
+  /// Waiting time
+  final int wait;
+
+  bool _isPolling = false;
+
+  /// Is listening running
+  bool get isPolling => _isPolling;
+
+  GroupLongpoll(this._api, {int? groupId, this.wait = 25})
+      : _groupId = groupId {
+    if (wait > 90) {
+      throw GroupLongpollException('Maximum value for timeout - 90');
+    }
+  }
+
+  @override
+  Future<void> start() async {
+    if (!_isPolling) {
+      _isPolling = true;
+
+      if (_groupId == null) {
+        final group = await _api.groups.getById();
+        _groupId = group['response'][0]['id'] as int;
+      }
+
+      return _api.groups.getLongPollServer(groupId: _groupId!).then((value) {
+        final res = value['response'];
+
+        return _startPolling(res['key'], res['server'], res['ts']);
+      });
+    } else {
+      throw GroupLongpollException('Longpoll is already running!');
+    }
+  }
+
+  @override
+  Future<void> stop() {
+    if (_isPolling) {
+      _isPolling = false;
+      _updateController.close();
+    }
+
+    return Future.value();
+  }
+
+  Future<void> _startPolling(String key, String server, String ts) {
+    if (_isPolling) {
+      Dio().get<Map<String, dynamic>>(
+        server,
+        queryParameters: {'act': 'a_check', 'key': key, 'ts': ts, 'wait': wait},
+      ).then((response) {
+        (response.data!['updates'] as List<dynamic>).forEach((element) {
+          emitUpdate(
+            UpdateGroupLongpoll.fromJson(element as Map<String, dynamic>),
+          );
+        });
+
+        _startPolling(key, server, response.data!['ts']);
+      });
+    }
+
+    return Future.value();
+  }
+
+  /// Returns new message events
+  Stream<MessageModel> onMessageNew() =>
+      onUpdate().where((event) => event.type == 'message_new').map(
+            (event) => MessageModel.fromJson(
+                event.object['message'] as Map<String, dynamic>)
+              ..clientInfo = event.object['client_info'],
+          );
+}
+
+class GroupLongpollException implements Exception {
+  final String message;
+
+  GroupLongpollException(this.message);
+
+  @override
+  String toString() => 'GroupLongpollException: $message';
+}
--- lib/src/longpoll/longpoll.dart
+++ lib/src/longpoll/longpoll.dart
@@ -5,6 +5,9 @@ import 'package:vk_library/vk_library.dart';
 
 export 'models.dart';
 
+part 'group_longpoll.dart';
+
+/// Main long poll class
 abstract class Longpoll<T> {
   final StreamController<T> _updateController;
 
@@ -15,92 +18,10 @@ abstract class Longpoll<T> {
 
   /// Adds a new event.
   void emitUpdate(T update) => _updateController.add(update);
-}
-
-class GroupLongpoll extends Longpoll<UpdateGroupLongpoll> {
-  final API _api;
-  int? _groupId;
-
-  /// Group Id.
-  int? get groupId => _groupId;
-
-  /// Waiting time
-  final int wait;
-
-  bool _isPolling = false;
-
-  /// Is listening running
-  bool get isPolling => _isPolling;
-
-  GroupLongpoll(this._api, {int? groupId, this.wait = 25})
-      : _groupId = groupId {
-    if (wait > 90) {
-      throw GroupLongpollException('Maximum value for timeout - 90');
-    }
-  }
 
   /// Starts listening for new updates
-  Future<void> start() async {
-    if (!_isPolling) {
-      _isPolling = true;
-
-      if (_groupId == null) {
-        final group = await _api.groups.getById();
-        _groupId = group['response'][0]['id'] as int;
-      }
-
-      final parameters =
-          await _api.groups.getLongPollServer(groupId: _groupId!);
-      final response = parameters['response'];
-
-      return _startPolling(response['key'], response['server'], response['ts']);
-    } else {
-      throw GroupLongpollException('Longpoll is already running!');
-    }
-  }
+  Future<void> start();
 
   /// Stop listening for new updates
-  Future<void> stop() {
-    if (_isPolling) {
-      _isPolling = false;
-      _updateController.close();
-    }
-
-    return Future.value();
-  }
-
-  Future<void> _startPolling(String key, String server, String ts) {
-    if (_isPolling) {
-      Dio().get<Map<String, dynamic>>(
-        server,
-        queryParameters: {'act': 'a_check', 'key': key, 'ts': ts, 'wait': wait},
-      ).then((response) {
-        (response.data!['updates'] as List<dynamic>).forEach((element) {
-          emitUpdate(
-            UpdateGroupLongpoll.fromJson(element as Map<String, dynamic>),
-          );
-        });
-
-        _startPolling(key, server, response.data!['ts']);
-      });
-    }
-
-    return Future.value();
-  }
-
-  Stream<MessageModel> onMessageNew() =>
-      onUpdate().where((event) => event.type == 'message_new').map(
-            (event) => MessageModel.fromJson(
-                event.object['message'] as Map<String, dynamic>)
-              ..clientInfo = event.object['client_info'],
-          );
-}
-
-class GroupLongpollException implements Exception {
-  final String message;
-
-  GroupLongpollException(this.message);
-
-  @override
-  String toString() => 'GroupLongpollException: $message';
+  Future<void> stop();
 }
